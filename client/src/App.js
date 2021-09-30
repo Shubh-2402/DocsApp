63c5bd01a3d8ab0ea86df094e7de95f8cafd547f
Shubh-2402 <56020434+Shubh-2402@users.noreply.github.com>
2021-09-30 10:20:50 +0530

    added login/register page

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import {StreamChat} from "stream-chat"
 import {Chat} from "stream-chat-react"
 import Cookies from "universal-cookie"
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer , Auth } from './components';
 import {} from "dotenv/config"
 
 const apiKey = process.env.REACT_APP_STREAM_API_KEY
 
 const chatClient = StreamChat.getInstance(apiKey);
 
+const authToken = false
+
 function App() {
+
+  if(!authToken){
+    return <Auth/>
+  }
 
   return (
     <div className="app__wrapper">
